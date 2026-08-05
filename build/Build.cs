e51e86a287f51944d0b1aff1292e5db6a42bb758
lukelanphear <luke@edgeofcrazy.com>
2026-08-05 10:48:22 -0700

    Upgraded Nuke to Fallout for the build system...

--- build/Build.cs
+++ build/Build.cs
@@ -1,9 +1,9 @@
-using Nuke.Common;
-using Nuke.Common.ProjectModel;
-using Automation.Nuke.Components;
-using Automation.Nuke.Components.Components;
-using Automation.Nuke.Components.DefaultBuilds;
-using Automation.Nuke.Components.Parameters;
+using Fallout.Common;
+using Fallout.Solutions;
+using Automation.Fallout.Components;
+using Automation.Fallout.Components.Components;
+using Automation.Fallout.Components.DefaultBuilds;
+using Automation.Fallout.Components.Parameters;
 
 /// <summary>
 /// Build configuration for PackageBuild
@@ -14,11 +14,11 @@ using Automation.Nuke.Components.Parameters;
 ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
 ///   - Microsoft VSCode           https://nuke.build/vscode
 
-public class Build : GitHubActionsBuild, IShowVersion, IClean, ICompile, IRestore, IScanForSecrets, IRunUnitTests, IRunIntegrationTests, IGenerateCoverageReport, ITest, IUpdateChangelog, IPackage, ITagRelease, IAnnounceRelease
+public class Build : GitHubActionsBuild, IShowVersion, IClean, ICompile, IRestore, IScanForSecrets, IRunUnitTests, IRunIntegrationTests, IGenerateCoverageReport, ITest, IUpdateChangelog, IPackageGitHub, ITagRelease, IAnnounceRelease
 {
 
     public static int Main() => Execute<Build>(
-        x => ((IPackage)x).ReleasePackage);
+        x => ((IPackageGitHub)x).ReleasePackage);
 
     int IHasTests.MinCoverageThreshold => 40;
     string IHasGitHubPackages.GitHubOwner => "meddlingidiot";
